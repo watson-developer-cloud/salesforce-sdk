b0886e7a43cd143f88578c1af4ee28b785955d60
Logan Patino <loganpatino10@gmail.com>
2017-10-16 16:12:22 -0400

    Uncomment actual deployment code

--- install/deploy.sh
+++ install/deploy.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-#if [ "{$TRAVIS_BRANCH}" = "master" ]; then
-  #ant -buildfile $TRAVIS_BUILD_DIR/install/build.xml deployWatson
-#fi
-
-ant -buildfile $TRAVIS_BUILD_DIR/install/build.xml deployWatsonCheckOnly
+if [ "{$TRAVIS_BRANCH}" = "master" ]; then
+  ant -buildfile $TRAVIS_BUILD_DIR/install/build.xml deployWatson
+fi
