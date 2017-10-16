4a20a66e194c92dc8454b1c566bedf3028ba5c39
Logan Patino <loganpatino10@gmail.com>
2017-10-16 15:17:01 -0400

    Uncomment tests and branch check

--- install/deploy.sh
+++ install/deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-#if [ "{$TRAVIS_BRANCH}" = "master" ]; then
-  #ant deployWatson
-#fi
-ant -buildfile $TRAVIS_BUILD_DIR/install/build.xml deployWatsonCheckOnly
+if [ "{$TRAVIS_BRANCH}" = "master" ]; then
+  ant -buildfile $TRAVIS_BUILD_DIR/install/build.xml deployWatson
+fi
